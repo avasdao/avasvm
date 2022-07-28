f38875e1ed54ca1415d2e80eb873e64ed1401767
Ava's DAO <support@avasdao.org>
2022-07-28 11:27:39 -0400

    Add new dataByKey method.

--- precompile/storage_gateway.go
+++ precompile/storage_gateway.go
@@ -20,14 +20,10 @@ import (
 
 var (
 	_                           StatefulPrecompileConfig = &StorageGatewayConfig{}
-	StorageGatewayPrecompile StatefulPrecompiledContract = createStorageGatewayPrecompile(StorageGatewayAddress)
+	StorageGatewayPrecompile StatefulPrecompiledContract = createStorageGatewayPrecompile()
 
-	sayHelloSignature     = CalculateFunctionSelector("sayHello()")
-
-	getData1Signature      = CalculateFunctionSelector("getData1(string)")
-	getData2Signature      = CalculateFunctionSelector("getData2(string,string)")
-	getData3Signature      = CalculateFunctionSelector("getData3(string)")
-	getData4Signature      = CalculateFunctionSelector("getData4(string,string)")
+	getDataSignature      = CalculateFunctionSelector("getData(string)")
+	getDataByKeySignature      = CalculateFunctionSelector("getDataByKey(string,string)")
 
 	setRecipientSignature = CalculateFunctionSelector("setRecipient(string,string)")
 
@@ -128,173 +124,7 @@ func SetRecipient(state StateDB, recipient string) {
 	state.SetState(StorageGatewayAddress, nameKey, common.BytesToHash([]byte(recipient)))
 }
 
-/**
- * sayHello is the execution function of "sayHello()"
- */
-func sayHello(
-	accessibleState PrecompileAccessibleState,
-	caller common.Address,
-	addr common.Address,
-	input []byte,
-	suppliedGas uint64,
-	readOnly bool,
-) (
-	ret []byte,
-	remainingGas uint64,
-	err error,
-) {
-	log.Info("\nEntering sayHello()")
-
-	if len(input) != 0 {
-		return nil, 0, fmt.Errorf("Oops! You cannot provide INPUT here.")
-	}
-
-	remainingGas, err = deductGas(suppliedGas, ReadStorageCost)
-
-	if err != nil {
-		return nil, 0, err
-	}
-
-	recipient := GetRecipient(accessibleState.GetStateDB())
-	log.Info("\nrecipient ->", recipient, nil)
-
-	testVal := "hi-there"
-
-	response := []byte(string(testVal))
-
-	// return []byte(fmt.Sprintf("Hello %s!", recipient)), remainingGas, nil
-	return response, remainingGas, nil
-}
-
-func getData1(precompileAddr common.Address) RunStatefulPrecompileFunc {
-	return func(
-		evm PrecompileAccessibleState,
-		callerAddr common.Address,
-		addr common.Address,
-		input []byte,
-		suppliedGas uint64,
-		readOnly bool,
-	) (
-		ret []byte,
-		remainingGas uint64,
-		err error,
-	) {
-		log.Info("input-1")
-		log.Info(string(input))
-
-		if remainingGas, err = deductGas(suppliedGas, ReadStorageCost); err != nil {
-			return nil, 0, err
-		}
-
-		const storageDataLength = 128
-
-		input = common.RightPadBytes(input, storageDataLength)
-		log.Info("input-2")
-		log.Info(string(input))
-
-		// testVal := "48d3c6d9-7324-4ae5-b7fd-32e19fa3996e"
-		// testVal := "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-
-		testVal := `{
-	      "chainId": 99999,
-	      "homesteadBlock": 0,
-	      "eip150Block": 0,
-	      "eip150Hash": "0x2086799aeebeae135c246c65021c82b4e15a2c451340993aacfd2751886514f0",
-	      "eip155Block": 0,
-	      "eip158Block": 0,
-	      "byzantiumBlock": 0,
-	      "constantinopleBlock": 0,
-	      "petersburgBlock": 0,
-	      "istanbulBlock": 0,
-	      "muirGlacierBlock": 0,
-	      "subnetEVMTimestamp": 0,
-	      "feeConfig": {
-	        "gasLimit": 20000000,
-	        "minBaseFee": 1000000000,
-	        "targetGas": 100000000,
-	        "baseFeeChangeDenominator": 48,
-	        "minBlockGasCost": 0,
-	        "maxBlockGasCost": 10000000,
-	        "targetBlockRate": 2,
-	        "blockGasCostStep": 500000
-	      },
-	      "storageGatewayConfig": {
-	          "blockTimestamp": 0
-	      }
-	    }`
-
-		response := []byte(string(testVal))
-
-		return response, remainingGas, nil
-		// return []byte(fmt.Sprintf(testVal)), remainingGas, nil
-	}
-}
-
-func getData2(precompileAddr common.Address) RunStatefulPrecompileFunc {
-	return func(
-		evm PrecompileAccessibleState,
-		callerAddr common.Address,
-		addr common.Address,
-		input []byte,
-		suppliedGas uint64,
-		readOnly bool,
-	) (
-		ret []byte,
-		remainingGas uint64,
-		err error,
-	) {
-		log.Info("input-1")
-		log.Info(string(input))
-
-		if remainingGas, err = deductGas(suppliedGas, ReadStorageCost); err != nil {
-			return nil, 0, err
-		}
-
-		const storageDataLength = 128
-
-		input = common.RightPadBytes(input, storageDataLength)
-		log.Info("input-2")
-		log.Info(string(input))
-
-		// testVal := "48d3c6d9-7324-4ae5-b7fd-32e19fa3996e"
-		// testVal := "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-
-		testVal := `{
-	      "chainId": 99999,
-	      "homesteadBlock": 0,
-	      "eip150Block": 0,
-	      "eip150Hash": "0x2086799aeebeae135c246c65021c82b4e15a2c451340993aacfd2751886514f0",
-	      "eip155Block": 0,
-	      "eip158Block": 0,
-	      "byzantiumBlock": 0,
-	      "constantinopleBlock": 0,
-	      "petersburgBlock": 0,
-	      "istanbulBlock": 0,
-	      "muirGlacierBlock": 0,
-	      "subnetEVMTimestamp": 0,
-	      "feeConfig": {
-	        "gasLimit": 20000000,
-	        "minBaseFee": 1000000000,
-	        "targetGas": 100000000,
-	        "baseFeeChangeDenominator": 48,
-	        "minBlockGasCost": 0,
-	        "maxBlockGasCost": 10000000,
-	        "targetBlockRate": 2,
-	        "blockGasCostStep": 500000
-	      },
-	      "storageGatewayConfig": {
-	          "blockTimestamp": 0
-	      }
-	    }`
-
-		response := []byte(string(testVal))
-
-		return response, remainingGas, nil
-		// return []byte(fmt.Sprintf(testVal)), remainingGas, nil
-	}
-}
-
-func getData3(
+func getData(
 	evm PrecompileAccessibleState,
 	callerAddr common.Address,
 	addr common.Address,
@@ -306,49 +136,14 @@ func getData3(
 	remainingGas uint64,
 	err error,
 ) {
-	log.Info("input-1")
+	log.Info("\ninput->")
 	log.Info(string(input))
 
 	if remainingGas, err = deductGas(suppliedGas, ReadStorageCost); err != nil {
 		return nil, 0, err
 	}
 
-	const storageDataLength = 128
-
-	input = common.RightPadBytes(input, storageDataLength)
-	log.Info("input-2")
-	log.Info(string(input))
-
-	// testVal := "48d3c6d9-7324-4ae5-b7fd-32e19fa3996e"
-	// testVal := "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-
-	testVal := `{
-      "chainId": 99999,
-      "homesteadBlock": 0,
-      "eip150Block": 0,
-      "eip150Hash": "0x2086799aeebeae135c246c65021c82b4e15a2c451340993aacfd2751886514f0",
-      "eip155Block": 0,
-      "eip158Block": 0,
-      "byzantiumBlock": 0,
-      "constantinopleBlock": 0,
-      "petersburgBlock": 0,
-      "istanbulBlock": 0,
-      "muirGlacierBlock": 0,
-      "subnetEVMTimestamp": 0,
-      "feeConfig": {
-        "gasLimit": 20000000,
-        "minBaseFee": 1000000000,
-        "targetGas": 100000000,
-        "baseFeeChangeDenominator": 48,
-        "minBlockGasCost": 0,
-        "maxBlockGasCost": 10000000,
-        "targetBlockRate": 2,
-        "blockGasCostStep": 500000
-      },
-      "storageGatewayConfig": {
-          "blockTimestamp": 0
-      }
-    }`
+	testVal := "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 	response := []byte(string(testVal))
 
@@ -356,7 +151,7 @@ func getData3(
 	// return []byte(fmt.Sprintf(testVal)), remainingGas, nil
 }
 
-func getData4(
+func getDataByKey(
 	evm PrecompileAccessibleState,
 	callerAddr common.Address,
 	addr common.Address,
@@ -368,49 +163,41 @@ func getData4(
 	remainingGas uint64,
 	err error,
 ) {
-	log.Info("input-1")
+	log.Info("\ninput->")
 	log.Info(string(input))
 
 	if remainingGas, err = deductGas(suppliedGas, ReadStorageCost); err != nil {
 		return nil, 0, err
 	}
 
-	const storageDataLength = 128
-
-	input = common.RightPadBytes(input, storageDataLength)
-	log.Info("input-2")
-	log.Info(string(input))
-
-	// testVal := "48d3c6d9-7324-4ae5-b7fd-32e19fa3996e"
-	// testVal := "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-
-	testVal := `{
-      "chainId": 99999,
-      "homesteadBlock": 0,
-      "eip150Block": 0,
-      "eip150Hash": "0x2086799aeebeae135c246c65021c82b4e15a2c451340993aacfd2751886514f0",
-      "eip155Block": 0,
-      "eip158Block": 0,
-      "byzantiumBlock": 0,
-      "constantinopleBlock": 0,
-      "petersburgBlock": 0,
-      "istanbulBlock": 0,
-      "muirGlacierBlock": 0,
-      "subnetEVMTimestamp": 0,
-      "feeConfig": {
-        "gasLimit": 20000000,
-        "minBaseFee": 1000000000,
-        "targetGas": 100000000,
-        "baseFeeChangeDenominator": 48,
-        "minBlockGasCost": 0,
-        "maxBlockGasCost": 10000000,
-        "targetBlockRate": 2,
-        "blockGasCostStep": 500000
-      },
-      "storageGatewayConfig": {
-          "blockTimestamp": 0
-      }
-    }`
+	testVal := "48d3c6d9-7324-4ae5-b7fd-32e19fa3996e"
+	// testVal := `{
+    //   "chainId": 99999,
+    //   "homesteadBlock": 0,
+    //   "eip150Block": 0,
+    //   "eip150Hash": "0x2086799aeebeae135c246c65021c82b4e15a2c451340993aacfd2751886514f0",
+    //   "eip155Block": 0,
+    //   "eip158Block": 0,
+    //   "byzantiumBlock": 0,
+    //   "constantinopleBlock": 0,
+    //   "petersburgBlock": 0,
+    //   "istanbulBlock": 0,
+    //   "muirGlacierBlock": 0,
+    //   "subnetEVMTimestamp": 0,
+    //   "feeConfig": {
+    //     "gasLimit": 20000000,
+    //     "minBaseFee": 1000000000,
+    //     "targetGas": 100000000,
+    //     "baseFeeChangeDenominator": 48,
+    //     "minBlockGasCost": 0,
+    //     "maxBlockGasCost": 10000000,
+    //     "targetBlockRate": 2,
+    //     "blockGasCostStep": 500000
+    //   },
+    //   "storageGatewayConfig": {
+    //       "blockTimestamp": 0
+    //   }
+    // }`
 
 	response := []byte(string(testVal))
 
@@ -460,20 +247,14 @@ func setRecipient(
  * Returns the StatefulPrecompile contract that implements
  * the StorageGateway interface from solidity.
  */
-func createStorageGatewayPrecompile(precompileAddr common.Address) StatefulPrecompiledContract {
+func createStorageGatewayPrecompile() StatefulPrecompiledContract {
 	/* Construct the contract without a fallback function. */
 	storageGatewayFuncs := []*statefulPrecompileFunction {
 		newStatefulPrecompileFunction(
-			sayHelloSignature, sayHello),
+			getDataSignature, getData),
 
 		newStatefulPrecompileFunction(
-			getData1Signature, getData1(precompileAddr)),
-		newStatefulPrecompileFunction(
-			getData2Signature, getData2(precompileAddr)),
-		newStatefulPrecompileFunction(
-			getData3Signature, getData3),
-		newStatefulPrecompileFunction(
-			getData4Signature, getData4),
+			getDataByKeySignature, getDataByKey),
 
 		newStatefulPrecompileFunction(
 			setRecipientSignature, setRecipient),
